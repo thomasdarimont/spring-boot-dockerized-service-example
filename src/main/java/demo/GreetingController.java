e4d91e1472e2126cebe0ea53dd1ed5422468678e
Thomas Darimont <thomas.darimont@gmail.com>
2017-05-17 23:34:57 +0200

    Add some gitinfo to response

--- src/main/java/demo/GreetingController.java
+++ src/main/java/demo/GreetingController.java
@@ -1,14 +1,28 @@
 package demo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
 class GreetingController {
 
-	@GetMapping("/")
-	String greet(@RequestParam(defaultValue = "World") String name) {
-		return "Hello " + name;
-	}
+  private final GitProperties gitProperties;
+
+  @GetMapping("/")
+  Object greet(@RequestParam(defaultValue = "World") String name) {
+    Map<String, Object> m = new LinkedHashMap<>();
+    m.put("greeting", "Hello " + name);
+    m.put("time", LocalDateTime.now());
+    m.put("commit", gitProperties.getShortCommitId());
+    m.put("commitTime", gitProperties.getCommitTime());
+    return m;
+  }
 }

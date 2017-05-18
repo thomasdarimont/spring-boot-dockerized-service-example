9c919396e78677f602822f009ec8ea7054002b73
Thomas Darimont <thomas.darimont@gmail.com>
2017-05-19 00:03:40 +0200

    CD Rocks

--- src/main/java/demo/GreetingController.java
+++ src/main/java/demo/GreetingController.java
@@ -2,17 +2,21 @@ package demo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.info.GitProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 class GreetingController {
+
+  private final Environment env;
 
   private final GitProperties gitProperties;
 
@@ -24,6 +28,7 @@ class GreetingController {
     m.put("commit", gitProperties.getShortCommitId());
     m.put("commitTime", gitProperties.getCommitTime());
     m.put("commitMessage", gitProperties.get("commit.message.short"));
+    m.put("active_profiles", Arrays.toString(env.getActiveProfiles()));
     return m;
   }
 }
